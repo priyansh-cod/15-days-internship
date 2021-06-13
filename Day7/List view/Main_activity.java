4462eda8a5b9d52f95faafeaeb19c6c2c3607e73
priyansh-cod <84833065+priyansh-cod@users.noreply.github.com>
2021-06-13 17:59:19 +0530

    Create Main_activity.java

---
+++ Day7/List view/Main_activity.java
@@ -0,0 +1,40 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    listView lv1;
+
+    String name[]={"av1","av2","av3","av4","av5","av6","av7","av8","av9","av10","av11","av12","av13","av14","av15","av16"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lv1=findViewById(R.id.lv1);
+
+
+
+     ArrayAdapter arrayAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,name);
+          lv1.setAdapter(arrayAdapter);
+
+          lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "You Selected" +name[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
