267f6cae89d7ee5ae93037b15f80200866e38c5c
priyansh-cod <84833065+priyansh-cod@users.noreply.github.com>
2021-06-13 17:27:51 +0530

    Create Splash2.java

---
+++ Day6/Splash 2/Splash2.java
@@ -0,0 +1,44 @@
+package com.example.splash1;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(getWindow().FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+        MediaPlayer ourSound;
+        ourSound = MediaPlayer.create(MainActivity.this,R.raw.y);
+        ourSound.start();
+        Thread thread=new Thread() {
+            public void run(){
+                try {
+                    sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
